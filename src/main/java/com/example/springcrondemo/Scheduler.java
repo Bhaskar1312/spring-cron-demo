682c32ab14b42489f25ca216675eb83bcb88268e
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2023-10-22 12:15:23 +0530

    Application Ready Event Listener starts on main thread and cron starts on schedduler thread

--- src/main/java/com/example/springcrondemo/Scheduler.java
+++ src/main/java/com/example/springcrondemo/Scheduler.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,12 @@ public class Scheduler {
     @Scheduled(cron = "0 */1 16 * * ?")
     public void someJob() {
         logger.info(LocalDateTime.now().toString());
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    // @Scheduled(cron = "0 */1 * * * *")
+    public void isAppReady() {
+        logger.info("Just printing Ready stuff");
     }
 
 }
